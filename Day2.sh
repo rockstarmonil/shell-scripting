e2b4b01ff94f5a948cbe35e8ff59a798619e6739
Monil Jain <82873328+rockstarmonil@users.noreply.github.com>
2025-04-02 15:24:52 +0530

    Update Day2.sh

--- Day2.sh
+++ Day2.sh
@@ -2,3 +2,6 @@ echo "*****************Welcome to the Linux Bash scripting**********************
 echo "What is your name?"
 read name
 echo "Hi $name, Have a wonderful day"
+
+echo "I'd like to ask if you want to create a user!"
+
