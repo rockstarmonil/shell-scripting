44743886d3a5c09c42690abe5d3b6b49b5125895
Monil Jain <moniljain0105@gmail.com>
2025-04-04 17:10:27 +0530

    removed unwanted code

--- Day2.sh
+++ Day2.sh
@@ -2,6 +2,3 @@ echo "*****************Welcome to the Linux Bash scripting**********************
 echo "What is your name?"
 read name
 echo "Hi $name, Have a wonderful day"
-
-echo "I'd like to ask if you want to create a user!"
-
