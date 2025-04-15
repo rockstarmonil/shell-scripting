7df4cb9e777336271a2f41099ac2beca3a829ec8
Monil Jain <moniljain0105@gmail.com>
2025-04-15 17:21:58 +0530

    created new script

---
+++ simple_backup_script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SOURCE_DIR="$HOME/Desktop/shell-scripting"
+BACKUP_DIR="$HOME/Desktop/Backup"
+DATE=$(date+%F)
+FILENAME="script-backup-$DATE.tar.gz"
+
+#-----Creating Backup Directory If Not Exists ------
+
+mkdir -p "$BACKUP_DIR"
+
+#-----Creating the Backup---------------------------
+
+tar -czvf "$BACKUP_DIR/$FILENAME" "$SOURCE_DIR"
+
+#-----Success Message------------------------------
+
+echo "Backup Created : $BACKUP_DIR/$FILENAME"
