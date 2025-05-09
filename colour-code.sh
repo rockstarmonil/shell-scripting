5fbe1fe28902cfa041d229820f85fbded0604bfb
Monil Jain <moniljain0105@gmail.com>
2025-05-09 17:56:37 +0530

    Colour-code script

---
+++ colour-code.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+#colour variables
+RED='\e[31m'
+GREEN='\e[32m'
+YELLOW='\e[33m'
+BLUE='\e[34m'
+
+
+#usage
+echo -e "${RED} This is Red Text"
+echo -e "${GREEN} This is Green Text"
+echo -e "${YELLOW} This is Yellow Text"
+echo -e "${BLUE} This is Blue Text"
